273bd0b6816ecae1b8182c614faa129aa94ec040
Esteplayer <estebansp08@gmail.com>
2026-02-18 19:25:27 -0500

    se agrego el repositorio del acudiente

---
+++ holamundo/src/main/java/edu/remington/holamundo/Repository/AcudienteRepository.java
@@ -0,0 +1,12 @@
+package edu.remington.holamundo.Repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import edu.remington.holamundo.model.Acudiente;
+
+
+public interface AcudienteRepository extends JpaRepository<Acudiente, Long> {
+
+    Optional<Acudiente> findByDocumento(String documento);
+    
+}
