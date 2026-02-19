6ba8481deaf9c3da854d5ab604bfb22e9f2948da
Esteplayer <estebansp08@gmail.com>
2026-02-18 19:39:37 -0500

    se arregla cambio

--- holamundo/src/main/java/edu/remington/holamundo/Repository/HistoriaClinicaRepository.java
+++ holamundo/src/main/java/edu/remington/holamundo/Repository/HistoriaClinicaRepository.java
@@ -1,12 +1,10 @@
 package edu.remington.holamundo.Repository;
 
 import java.util.Optional;
-
-import org.springframework.boot.data.autoconfigure.web.DataWebProperties.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import edu.remington.holamundo.model.HistoriaClinica;
 
 public interface HistoriaClinicaRepository extends JpaRepository<HistoriaClinica, Long> {
     
-    Optional<HistoriaClinica> findByAnimalId(Long animalId, Pageable pageable);
+    Optional<HistoriaClinica> findByAnimalId(Long animalId);
 }
